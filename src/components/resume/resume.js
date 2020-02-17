29e4d4885916f7dce41e20875f2cc581a1b6d9a2
Daniel <dantay0803@hotmail.co.uk>
2020-02-17 23:25:42 +0000

    Finished Resume Layout and added download button.

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Container } from 'react-bootstrap';
+import { MdFileDownload } from 'react-icons/md';
 import danieltaylorResume from '../../files/DanielTaylor_Resume.pdf';
 import Experience from './experience';
 import Education from './education';
@@ -9,6 +10,13 @@ import Skills from './skills';
 
 const Styles = styled.div`
   text-align: left;
+
+  .resumeContent {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
   .resumeTitle,
   .resumeDetails {
@@ -29,8 +37,8 @@ const Styles = styled.div`
 
   .resumeDetails {
     width: 850px;
-    /* border-left: 1px solid var(--text-muted);
-    padding-left: 10rem; */
+    border-left: 1px solid var(--text-muted);
+    padding-left: 10rem;
   }
 
   .resumeDate {
@@ -40,25 +48,60 @@ const Styles = styled.div`
 
   svg {
     stroke-width: 1;
-    margin-right: 0.75rem;
+    margin-right: 0.5rem;
+    color: var(--highlight);
+  }
+
+  .resumeDownload {
+    border: 2px solid var(--text-primary);
+    background: transparent;
+    width: 55px;
+    height: 55px;
+    overflow: hidden;
+    cursor: pointer;
+    transition: width 0.5s ease-in-out;
+  }
+
+  .resumeDownload svg,
+  .resumeDownload p {
+    color: var(--text-primary);
+    font-weight: bold;
+    float: left;
+    margin: 10px;
+  }
+
+  .resumeDownload:hover {
+    border-color: var(--highlight);
+    width: 225px;
+    transition: width 0.5s ease-in-out;
+  }
+
+  .resumeDownload:hover svg {
     color: var(--highlight);
   }
 `;
 
 export default function resume() {
-  const handleOptionChange = changeEvent => {
-    console.log(changeEvent);
-  };
-
   return (
     <Styles>
       <Container key="resume" id="resume" fluid>
         <h1>Resume</h1>
         <hr className="hrTitle" />
-        <Experience />
-        <Education />
-        <Awards />
-        <Skills />
+        <div className="resumeContent">
+          <a
+            href={danieltaylorResume}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="resumeDownload"
+          >
+            <MdFileDownload size={32} />
+            <p>DOWNLOAD</p>
+          </a>
+          <Experience />
+          <Education />
+          <Awards />
+          <Skills />
+        </div>
       </Container>
     </Styles>
   );
