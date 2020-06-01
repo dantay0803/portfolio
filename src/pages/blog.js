f792c100a3281106763ef30b092b43f791f8a8f5
Daniel <dantay0803@hotmail.co.uk>
2020-06-01 01:19:21 +0100

    Move Blog Page CSS to Blog Page Component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,81 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
 import BlogHeader from '../components/blog/blogHeader';
 import BlogCard from '../components/blog/blogCard';
+import Navbar from '../components/blog/blogNavbar';
+
+const Styles = styled.div`
+  margin: 0;
+  padding: 0;
+
+  .blogHeaderTitle {
+    font-size: 1.85rem;
+    font-weight: bold;
+    text-transform: none;
+    font-family: 'Montserrat', sans-serif;
+    line-height: 3rem;
+    margin: 0;
+    padding: 2rem 0 0 1rem;
+    width: 22rem;
+  }
+
+  .blogHeaderTitle {
+    padding: 0;
+    text-align: center;
+  }
+
+  .blogGrid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(325px, 525px));
+    justify-content: center;
+    justify-items: center;
+    grid-gap: 2em;
+    padding: 1rem;
+  }
+`;
+
+const BlogHome = ({ data }) => {
+  const posts = data.allMdx.edges;
+
+  return (
+    <Layout>
+      <SEO
+        title="Blog"
+        description="The personal bog of Daniel Taylor software engineer that covers various software development, career and personal topics"
+        pathname="/blog/"
+      />
+      <Styles>
+        <Navbar />
+        <BlogHeader />
+        <Container fluid>
+          <div className="blogGrid">
+            {posts.map(post => (
+              <BlogCard
+                key={post.node.frontmatter.slug}
+                thumbnail={
+                  post.node.frontmatter.featuredImage.childImageSharp.fluid
+                    .src ||
+                  `https://via.placeholder.com/185x278?text=Image+not+available`
+                }
+                alt={post.node.frontmatter.featuredImage.altText}
+                postTitle={post.node.frontmatter.title}
+                postDate={post.node.frontmatter.date}
+                postDescription={post.node.frontmatter.excerpt}
+                postPath={`/${post.node.frontmatter.slug}`}
+                categories={post.node.frontmatter.category}
+              />
+            ))}
+          </div>
+        </Container>
+      </Styles>
+    </Layout>
+  );
+};
+
+export default BlogHome;
 
 export const query = graphql`
   query BlogPageQuery {
@@ -30,38 +102,3 @@ export const query = graphql`
     }
   }
 `;
-
-const BlogHome = ({ data }) => {
-  const posts = data.allMdx.edges;
-
-  return (
-    <Layout>
-      <SEO
-        title="Blog"
-        description="The personal bog of Daniel Taylor software engineer that covers various software development, career and personal topics"
-        pathname="/blog/"
-      />
-      <BlogHeader />
-      <Container fluid>
-        <div className="blogGrid">
-          {posts.map(post => (
-            <BlogCard
-              key={post.node.frontmatter.slug}
-              thumbnail={
-                post.node.frontmatter.featuredImage.childImageSharp.fluid.src ||
-                `https://via.placeholder.com/185x278?text=Image+not+available`
-              }
-              alt={post.node.frontmatter.featuredImage.altText}
-              postTitle={post.node.frontmatter.title}
-              postDate={post.node.frontmatter.date}
-              postDescription={post.node.frontmatter.excerpt}
-              postPath={`/${post.node.frontmatter.slug}`}
-              categories={post.node.frontmatter.category}
-            />
-          ))}
-        </div>
-      </Container>
-    </Layout>
-  );
-};
-export default BlogHome;
