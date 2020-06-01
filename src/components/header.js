b9e827dd61f9b4aedec8cc2e7951c2d5a049fdd5
Daniel <dantay0803@hotmail.co.uk>
2020-06-01 01:18:55 +0100

    Add Call to Action to Header and Update

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Container, Jumbotron, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-scroll';
 import headerImage from '../assets/images/headerImage.jpg';
 
 const StyledJumbotron = styled(Jumbotron)`
@@ -10,7 +11,7 @@ const StyledJumbotron = styled(Jumbotron)`
   background-size: cover;
   height: 100vh;
   padding: 0;
-  margin: 0;
+  margin: 3.5em 0 0 0;
   overflow: hidden;
 
   .container-fluid{
@@ -36,6 +37,102 @@ const StyledJumbotron = styled(Jumbotron)`
     position: absolute;
     right: 0;
   }
+
+  h1 {
+    font-size: 1.6rem;
+    font-weight: bold;
+    text-transform: none;
+    font-family: 'Montserrat', sans-serif;
+    line-height: 3rem;
+    margin: 0;
+    padding: 1.75rem 0 0 1rem;
+    width: 18rem;
+    margin-bottom: 0.75em;
+  }
+
+  .actionLink {
+    width: 9em;
+    padding: 0.65em;
+    margin: 0 0 1em 1em;
+    font-weight: bold;
+    font-size: 0.9rem;
+    display: block;
+    text-align: center;
+  }
+
+  .actionLink:hover {
+    cursor: pointer;
+  }
+
+  .cta {
+    background-color: var(--highlight);
+    border: 1px solid var(--highlight);
+    color: var(--text-secondary);
+  }
+
+  .contact {
+    background-color: transparent;
+    border: 1px solid var(--text-muted);
+    color: var(--text-primary);
+  }
+
+  .contact:hover{
+    background-color: var(--text-muted);
+  }
+
+  @media (min-width: 576px) {
+    h1{
+      font-size: 2.25rem;
+      padding: 5rem 0 0 1rem;
+      width: 30rem;
+    }
+
+    .actionLink {
+      display: inline;
+    }
+  }
+
+  @media (min-width: 768px) {
+    h1{
+      font-size: 3rem;
+      line-height: 4.75rem;
+      width: 40rem;
+    }
+
+    .actionLink {
+      width: 9em;
+      height: 2.5em;
+      padding: 0.75em;
+      margin: 0 0 0 1em;
+      font-weight: bold;
+      font-size: 1.2rem;
+      display: inline;
+    }
+  }
+
+  @media (min-width: 992px) {
+    h1{
+      font-size: 3.875rem;
+      width: 51rem;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    h1 {
+      font-size: 3.75rem;
+      line-height: 4.75rem;
+      width: 54rem;
+      padding-top: 7.5rem;
+    }
+  }
+
+  @media (min-width: 1400px) {
+    h1 {
+      font-size: 3.875rem;
+      width: 70rem;
+      padding-top: 10rem;
+    }
+  }
 `;
 
 export default function Header() {
@@ -45,10 +142,30 @@ export default function Header() {
         <Row noGutters>
           <Col xs={6}>
             <div className="black">
-              <h1 className="headerTitle">
-                I build high-quality web & mobile applications
+              <h1>
+                I build high-quality web & mobile products that bring your users
+                & products closer.
               </h1>
-              <h2 className="headerSubtitle">Software Developer</h2>
+              <Link
+                className="actionLink cta"
+                to="projects"
+                spy
+                smooth
+                offset={-30}
+                duration={500}
+              >
+                View My Work
+              </Link>
+              <Link
+                className="actionLink contact"
+                to="contact"
+                spy
+                smooth
+                offset={-30}
+                duration={500}
+              >
+                Contact Me
+              </Link>
             </div>
           </Col>
           <Col xs={6}>
