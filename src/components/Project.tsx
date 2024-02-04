f4f1ecbead16e2ead7814250866d6b0a05c754a3
Daniel <dantay0803@hotmail.co.uk>
2024-02-04 23:17:21 +0000

    fix: projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -6,13 +6,14 @@ type ProjectProps = {
   description: string
   image: React.ReactNode
   reverse: boolean
+  onClick?: () => void
 }
 
-const Project = ({title, tools, description, image, reverse}: ProjectProps) => {
+const Project = ({title, tools, description, image, reverse, onClick}: ProjectProps) => {
   return (
-    <div className={`w-full flex flex-col gap-y-1 ${reverse ? 'justify-end text-right' : 'justify-start text-left'}`}>
+    <div onClick={onClick} className={`w-full flex flex-col gap-y-1 ${reverse ? 'justify-end text-right' : 'justify-start text-left'} hover:cursor-pointer`}>
       <p className='text-xl text-black underline decoration-accent font-medium'>{title}</p>
-      <p className='text-sm text-accent-dark font-normal italic'>{tools.map((tool, index) => (<span>{tool}{index !== tools.length - 1 && ', '}</span>))}</p>
+      <p className='text-sm text-accent-dark font-normal italic'>{tools.map((tool, index) => (<span key={tool}>{tool}{index !== tools.length - 1 && ', '}</span>))}</p>
       <p>{description}</p>
       {image}
     </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,46 +1,99 @@
-import { StaticImage } from 'gatsby-plugin-image'
-import React from 'react'
-import Project from './Project'
-
-const PROJECTS = [
-  {
-    title: 'Phalanx',
-    tools: ['JavaScript', 'Cordova', 'C#', 'ASP.NET', 'SQL'],
-    description: 'PHALANX is an integrated operations software application that combines RFID/barcode technology, operational mobility with the use of smartphones and tablets and integration with back-office systems on a single software platform.',
-    image: <StaticImage className='w-full h-64 object-cover' src={'../assets/images/thumbnails/thumbnail_phalanx.jpg'} alt="Phalanx project thumbnail" />
-  },
-  {
-    title: 'Planet Library',
-    tools: ['C#', 'Unity'],
-    description: 'Planet Library transforms your Library into a fun and engaging digital space for children.',
-    image: <StaticImage className='w-full h-64 object-cover' src={'../assets/images/thumbnails/thumbnail_pl.jpg'} alt="Planet Library project thumbnail" />
-  },
-  {
-    title: 'What to Watch',
-    tools: ['React', 'React Router', 'Javascript', 'Bootstrap', 'Styled Components'],
-    description: 'A TV show and movie searching website created with React and powered by the MovieDB API.',
-    image: <StaticImage className='w-full h-64 object-cover' src={'../assets/images/thumbnails/thumbnail_whatToWatch.jpg'} alt="Planet Library project thumbnail" />
-  },
-  {
-    title: 'Clandestine Ops',
-    tools: ['C#', 'Unity'],
-    description: 'A First Person Shooter demo created to aid with research into the effect of violent video game exposure to users.',
-    image: <StaticImage className='w-full h-64 object-cover' src={'../assets/images/thumbnails/thumbnail_co.jpg'} alt="Planet Library project thumbnail" />
-  },
-]
+import { StaticImage } from "gatsby-plugin-image"
+import React, { useState } from "react"
+import Project from "./Project"
+import Phalanx from "./projectDetails/Phalanx"
 
 const Projects = () => {
+  const [isPhalanxModalOpen, setIsPhalanxModalOpen] = useState(false)
+
+  const PROJECTS = [
+    {
+      title: "Phalanx",
+      tools: ["JavaScript", "Cordova", "C#", "ASP.NET", "SQL"],
+      description:
+        "PHALANX is an integrated operations software application that combines RFID/barcode technology, operational mobility with the use of smartphones and tablets and integration with back-office systems on a single software platform.",
+      image: (
+        <StaticImage
+          className="w-full h-64 object-cover"
+          src={"../assets/images/thumbnails/thumbnail_phalanx.jpg"}
+          alt="Phalanx project thumbnail"
+        />
+      ),
+      onClick: () => setIsPhalanxModalOpen(true),
+    },
+    {
+      title: "Planet Library",
+      tools: ["C#", "Unity"],
+      description:
+        "Planet Library transforms your Library into a fun and engaging digital space for children.",
+      image: (
+        <StaticImage
+          className="w-full h-64 object-cover"
+          src={"../assets/images/thumbnails/thumbnail_pl.jpg"}
+          alt="Planet Library project thumbnail"
+        />
+      ),
+    },
+    {
+      title: "What to Watch",
+      tools: [
+        "React",
+        "React Router",
+        "Javascript",
+        "Bootstrap",
+        "Styled Components",
+      ],
+      description:
+        "A TV show and movie searching website created with React and powered by the MovieDB API.",
+      image: (
+        <StaticImage
+          className="w-full h-64 object-cover"
+          src={"../assets/images/thumbnails/thumbnail_whatToWatch.jpg"}
+          alt="Planet Library project thumbnail"
+        />
+      ),
+    },
+    {
+      title: "Clandestine Ops",
+      tools: ["C#", "Unity"],
+      description:
+        "A First Person Shooter demo created to aid with research into the effect of violent video game exposure to users.",
+      image: (
+        <StaticImage
+          className="w-full h-64 object-cover"
+          src={"../assets/images/thumbnails/thumbnail_co.jpg"}
+          alt="Planet Library project thumbnail"
+        />
+      ),
+    },
+  ]
+
   return (
-    <div id="projects" className='w-full bg-secondary'>
-      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-5 lg:pb-10'>
-        <div className='mx-auto max-w-2xl flex-shrink-0 md:mx-0 pb-5 pt-5 lg:pt-10'>
-          <p className='text-accent-dark text-lg underline'>Things I've worked on solo and as part of a team</p>
-          <h2 className='uppercase text-black text-6xl mb-8 font-semibold'>Projects</h2>
+    <div id="projects" className="w-full bg-secondary">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-5 lg:pb-10">
+        <div className="mx-auto max-w-2xl flex-shrink-0 md:mx-0 pb-5 pt-5 lg:pt-10">
+          <p className="text-accent-dark text-lg underline">
+            Things I've worked on solo and as part of a team
+          </p>
+          <h2 className="uppercase text-black text-6xl mb-8 font-semibold">
+            Projects
+          </h2>
         </div>
-        <div className='w-full flex flex-col gap-y-10'>
-          {PROJECTS.map((project, index) => (<Project key={project.title} title={project.title} tools={project.tools} description={project.description} image={project.image} reverse={index % 2 !== 0} />))}
+        <div className="w-full flex flex-col gap-y-10">
+          {PROJECTS.map((project, index) => (
+            <Project
+              key={project.title}
+              title={project.title}
+              tools={project.tools}
+              description={project.description}
+              image={project.image}
+              reverse={index % 2 !== 0}
+              onClick={project.onClick}
+            />
+          ))}
         </div>
       </div>
+      <Phalanx isOpen={isPhalanxModalOpen} setIsOpen={setIsPhalanxModalOpen} />
     </div>
   )
 }
