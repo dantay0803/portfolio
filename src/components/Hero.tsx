cd5b8fd85378a4dbef56ea5e1008f125e5b8092e
Daniel <dantay0803@hotmail.co.uk>
2024-02-06 23:27:59 +0000

    style: add padding top

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="h-screen overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36 hero">
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-        <div className='mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:pt-10'>
+        <div className='mx-auto max-w-2xl flex-shrink-0 lg:mx-0 pt-10'>
           <p className='text-white-500 text-lg underline'>Software Developer</p>
           <div className='flex flex-row justify-start items-end gap-x-4 mb-8'>
             <h1 className='uppercase text-white text-4xl sm:text-6xl font-bold w-min md:w-max'>Daniel Taylor</h1>
