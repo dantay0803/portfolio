73120d38abcfc7a1eb1bd48d05572b301c6c6414
Daniel <dantay0803@hotmail.co.uk>
2025-10-24 00:02:54 +0100

    fix: scroll to top component

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,34 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const ScrollToTop = () => {
-  const [showComponent, setShowComponent] = useState(false)
-
+  const [showComponent, setShowComponent] = useState(false);
 
   const handleSetVisible = () => {
-    if (window.scrollY > 100) {
-      setShowComponent(true)
+    if (typeof window !== "undefined" && window.scrollY > 100) {
+      setShowComponent(true);
     } else {
-      setShowComponent(false)
+      setShowComponent(false);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleSetVisible);
-    return () => {
-      window.removeEventListener('scroll', handleSetVisible);
-    };
-  }, [window.scrollY])
-  
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", handleSetVisible);
+      return () => {
+        window.removeEventListener("scroll", handleSetVisible);
+      };
+    }
+  }, []);
 
   const handleClick = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  }
+    if (typeof window !== "undefined") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
 
   return (
-    <button className={`${window.scrollY < 100 ? 'hidden' : 'fixed'} bottom-5 right-5 z-10 opacity-50 hover:opacity-100 rounded-full bg-accent px-3 py-2`} onClick={handleClick} aria-label='Scroll to top'>
+    <button
+      className={`${
+        !showComponent ? "hidden" : "fixed"
+      } bottom-5 right-5 z-10 opacity-50 hover:opacity-100 rounded-full bg-accent px-3 py-2`}
+      onClick={handleClick}
+      aria-label="Scroll to top"
+    >
       👆
     </button>
-  )
-}
+  );
+};
 
-export default ScrollToTop
+export default ScrollToTop;
