bcb3c118692a09040d7213744495c9ca9cff19fb
Daniel <dantay0803@hotmail.co.uk>
2025-10-24 10:59:34 +0100

    fix: blog card image

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -23,7 +23,7 @@ const BlogCard = ({
 
   return (
     <Link to={slug}>
-      <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-6 pb-8 pt-80 sm:pt-48 lg:pt-80 h-[440px] hover:scale-105 transform transition duration-500">
+      <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 h-[440px] hover:scale-105 transform transition duration-500">
         {featuredImg && (
           <GatsbyImage
             image={featuredImg}
@@ -37,7 +37,7 @@ const BlogCard = ({
           />
         )}
         <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
-        <div className="w-full h-full flex flex-col justify-end gap-y-2 text-white">
+        <div className="w-full h-full flex flex-col justify-end gap-y-2 text-white px-6 pb-8 pt-80 sm:pt-48 lg:pt-80">
           <div className="flex flex-row justify-between items-center italic text-xs">
             <p>{date}</p>
             <p className="text-white-300 rounded-lg px-2 py-1">{categories}</p>
